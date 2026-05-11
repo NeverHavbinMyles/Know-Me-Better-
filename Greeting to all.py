b54efbddc36753ba76fdc7bcb53590424505426b
NeverHavbinMyles <mdabadejos@gmail.com>
2026-05-11 11:38:13 +0800

    Add files via upload

---
+++ Greeting to all.py
@@ -0,0 +1,17 @@
+
+secretmessage = "NeverHavbinMyles"
+
+message = input("Good day to you, how are you??    ")
+
+if message == secretmessage:
+    print("""This project hazbin posted through GitHub through the steps of instructions on 'How to start a project on Github' by both my school, and the internet.
+The problem I face the most is on how to actually submit this. This code was simple for me to make. When submitting this, I took 2 hours in learning how to submit this.
+After I submitted this, this made me realize that cloud hosting services, such as this, really makes my project  "more useful to others as it is accesible, making it reliable.
+I have no space left to finish this code so goodbye!!""")
+
+else:
+    print("That's great!!!")
+
+
+input("Enter enter to exit")
+          
